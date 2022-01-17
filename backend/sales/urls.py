7661d74a660c74505d4e4abf7cc12dd5ff1bf041
unniznd <aanandunnis@gmail.com>
2022-01-17 15:27:28 +0530

    changes

--- backend/sales/urls.py
+++ backend/sales/urls.py
@@ -4,7 +4,6 @@ from .views import SaleView, BillView
 
 urlpatterns = [
     path('bill/<int:c>/',BillView.as_view()),
-    path('bill/',BillView.as_view()),
     path('<int:c>/',SaleView.as_view()),
     path('',SaleView.as_view())
 ]
